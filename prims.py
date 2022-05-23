453d8b622a990452fa0b211ca10a3a928ead23c5
Shrikant <98737175+ShrikantSarode@users.noreply.github.com>
2022-05-23 08:09:48 +0530

    Update prims.py

--- prims.py
+++ prims.py
@@ -1,74 +1,35 @@
-def isSafe(graph, color):
-   
-    # check for every edge
-    for i in range(4):
-        for j in range(i + 1, 4):
-            if (graph[i][j] and color[j] == color[i]):
-                return False
-    return True
- 
-# /* This function solves the m Coloring
-# problem using recursion. It returns
-# false if the m colours cannot be assigned,
-# otherwise, return true and prints
-# assignments of colours to all vertices.
-# Please note that there may be more than
-# one solutions, this function prints one
-# of the feasible solutions.*/
-def graphColoring(graph, m, i, color):
-   
-    # if current index reached end
-    if (i == 4):
- 
-        # if coloring is safe
-        if (isSafe(graph, color)):
- 
-            # Print the solution
-            printSolution(color)
-            return True
-        return False
- 
-    # Assign each color from 1 to m
-    for j in range(1, m + 1):
-        color[i] = j
- 
-        # Recur of the rest vertices
-        if (graphColoring(graph, m, i + 1, color)):
-            return True
-        color[i] = 0
-    return False
- 
-# /* A utility function to print solution */
-def printSolution(color):
-    print("Solution Exists:" " Following are the assigned colors ")
-    for i in range(4):
-        print(color[i],end=" ")
- 
-# Driver code
-if __name__ == '__main__':
-   
-    # /* Create following graph and
-    # test whether it is 3 colorable
-    # (3)---(2)
-    # | / |
-    # | / |
-    # | / |
-    # (0)---(1)
-    # */
-    graph = [
-        [ 0, 1, 1, 1 ],
-        [ 1, 0, 1, 0 ],
-        [ 1, 1, 0, 1 ],
-        [ 1, 0, 1, 0 ],
-    ]
-    m = 3 # Number of colors
- 
-    # Initialize all color values as 0.
-    # This initialization is needed
-    # correct functioning of isSafe()
-    color = [0 for i in range(4)]
- 
-    if (not graphColoring(graph, m, 0, color)):
-        print ("Solution does not exist")
- 
-# This code is contributed by mohit kumar 29
+INF = 9999999
+# number of vertices in graph
+N = 5
+#creating graph by adjacency matrix method
+G = [[0, 19, 5, 0, 0],
+     [19, 0, 5, 9, 2],
+     [5, 5, 0, 1, 6],
+     [0, 9, 1, 0, 1],
+     [0, 2, 6, 1, 0]]
+
+selected_node = [0, 0, 0, 0, 0]
+
+no_edge = 0
+
+selected_node[0] = True
+
+# printing for edge and weight
+print("Edge : Weight\n")
+while (no_edge < N - 1):
+    
+    minimum = INF
+    a = 0
+    b = 0
+    for m in range(N):
+        if selected_node[m]:
+            for n in range(N):
+                if ((not selected_node[n]) and G[m][n]):  
+                    # not in selected and there is an edge
+                    if minimum > G[m][n]:
+                        minimum = G[m][n]
+                        a = m
+                        b = n
+    print(str(a) + "-" + str(b) + ":" + str(G[a][b]))
+    selected_node[b] = True
+    no_edge += 1
